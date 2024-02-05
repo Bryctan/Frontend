6ea615ccd6a10146b5c39f70e92ee1eabccee1cb
Juan Camilo Quintero Jimenez <110494864+Bryctan@users.noreply.github.com>
2024-02-05 00:02:37 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    url_auth: 'http://localhost:5001',
-    url_logic: 'http://localhost:4001'
+    url_auth: 'https://auth-service-tu0i.onrender.com',
+    url_logic: 'https://business-services.onrender.com'
 };
