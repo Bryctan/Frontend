a388b8599bd9143c484adf14f750c87e3b300d5c
Juan Camilo Quintero Jimenez <110494864+Bryctan@users.noreply.github.com>
2024-02-05 19:12:23 -0500

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -98,7 +98,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,{hash:true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
